356de81d8a131a8129608334ef24f610795f740d
Rika Nicole <mzpf2288pc@privaterelay.appleid.com>
2026-09-02 08:24:58 -0400

    Removed Constructor

--- ver2/Account.java
+++ ver2/Account.java
@@ -4,10 +4,7 @@ public class Account {
     private double balance;// New Comment for balance
     private String name; 
 
-    // Constructor
-    public Account(double balance) {
-        this("Unknown", balance);
-    }
+    
 
     public Account(String name, double balance) {
         this.name = name;
